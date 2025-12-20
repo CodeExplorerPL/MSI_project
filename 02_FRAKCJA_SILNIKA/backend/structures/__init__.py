5448aa6967b8960ad5944aaec104c0d24019ed5f
wojRz <wojtek.sqwarku@gmail.com>
2025-12-20 21:33:46 +0100

    Added map generator with some pregenerated maps

---
+++ 02_FRAKCJA_SILNIKA/backend/structures/__init__.py
@@ -0,0 +1,44 @@
+""" Inicjalizacja pakietu struktur """
+
+from .map_info import MapInfo
+from .map_loader import MapLoader
+from .obstacle import (
+    Obstacle,
+    ObstacleType,
+    Wall,
+    Tree,
+    AntiTankSpike,
+    ObstacleUnion
+)
+from .position import Position
+from .powerup import PowerUpData, PowerUpType
+from .terrain import (
+    Terrain,
+    Grass,
+    Road,
+    Swamp,
+    PotholeRoad,
+    Water,
+    TerrainUnion
+)
+
+__all__ = [
+    "MapInfo",
+    "MapLoader",
+    "Obstacle",
+    "ObstacleType",
+    "Wall",
+    "Tree",
+    "AntiTankSpike",
+    "ObstacleUnion",
+    "Position",
+    "PowerUpData",
+    "PowerUpType",
+    "Terrain",
+    "Grass",
+    "Road",
+    "Swamp",
+    "PotholeRoad",
+    "Water",
+    "TerrainUnion",
+]
--- 02_FRAKCJA_SILNIKA/backend/structures/terrain.py
+++ 02_FRAKCJA_SILNIKA/backend/structures/terrain.py
@@ -1,58 +1,64 @@
 """ Klasa terenu """
+from abc import ABC
+from dataclasses import dataclass, field
+from typing import Union, List, Literal
+
+from .position import Position
 
 
-# # --- TERENY (Terrains) ---
-# @dataclass
-# class Terrain(ABC):
-#     """Abstrakcyjna klasa bazowa dla typów terenu."""
-#     id: str
-#     position: Position
-#     size: List[int] = field(default_factory=lambda: [10, 10])
-#
-#     terrain_type: str = field(init=False)
-#     movement_speed_modifier: float = field(init=False)
-#     deal_damage: int = field(init=False)
-#
-#
-# @dataclass
-# class Grass(Terrain):
-#     """Trawa: Brak efektu."""
-#     terrain_type: Literal["Grass"] = field(default="Grass", init=False)
-#     movement_speed_modifier: float = 1
-#     deal_damage: int = 0
-#
-#
-# @dataclass
-# class Road(Terrain):
-#     """Droga: Zwiększa prędkość ruchu."""
-#     terrain_type: Literal["Road"] = field(default="Road", init=False)
-#     movement_speed_modifier: float = 1.5
-#     deal_damage: int = 0
-#
-#
-# @dataclass
-# class Swamp(Terrain):
-#     """Bagno: Spowalnia ruch."""
-#     terrain_type: Literal["Swamp"] = field(default="Swamp", init=False)
-#     movement_speed_modifier: float = 0.4
-#     deal_damage: int = 0
-#
-#
-# @dataclass
-# class PotholeRoad(Terrain):
-#     """Droga z Dziurami: Spowalnia i zadaje minimalne obrażenia."""
-#     terrain_type: Literal["PotholeRoad"] = field(default="PotholeRoad",
-#                                                  init=False)
-#     movement_speed_modifier: float = 0.95
-#     deal_damage: int = 5
-#
-#
-# @dataclass
-# class Water(Terrain):
-#     """Woda: Spowalnia i zadaje obrażenia."""
-#     terrain_type: Literal["Water"] = field(default="Water", init=False)
-#     movement_speed_modifier: float = 0.7
-#     deal_damage: int = 10
-#
-#
-# TerrainUnion = Union[Grass, Road, Swamp, PotholeRoad, Water]
+
+# --- TERENY (Terrains) ---
+@dataclass
+class Terrain(ABC):
+    """Abstrakcyjna klasa bazowa dla typów terenu."""
+    id: str
+    position: Position
+    size: List[int] = field(default_factory=lambda: [10, 10])
+
+    terrain_type: str = field(init=False)
+    movement_speed_modifier: float = field(init=False)
+    deal_damage: int = field(init=False)
+
+
+@dataclass
+class Grass(Terrain):
+    """Trawa: Brak efektu."""
+    terrain_type: Literal["Grass"] = field(default="Grass", init=False)
+    movement_speed_modifier: float = 1
+    deal_damage: int = 0
+
+
+@dataclass
+class Road(Terrain):
+    """Droga: Zwiększa prędkość ruchu."""
+    terrain_type: Literal["Road"] = field(default="Road", init=False)
+    movement_speed_modifier: float = 1.5
+    deal_damage: int = 0
+
+
+@dataclass
+class Swamp(Terrain):
+    """Bagno: Spowalnia ruch."""
+    terrain_type: Literal["Swamp"] = field(default="Swamp", init=False)
+    movement_speed_modifier: float = 0.4
+    deal_damage: int = 0
+
+
+@dataclass
+class PotholeRoad(Terrain):
+    """Droga z Dziurami: Spowalnia i zadaje minimalne obrażenia."""
+    terrain_type: Literal["PotholeRoad"] = field(default="PotholeRoad",
+                                                 init=False)
+    movement_speed_modifier: float = 0.95
+    deal_damage: int = 5
+
+
+@dataclass
+class Water(Terrain):
+    """Woda: Spowalnia i zadaje obrażenia."""
+    terrain_type: Literal["Water"] = field(default="Water", init=False)
+    movement_speed_modifier: float = 0.7
+    deal_damage: int = 10
+
+
+TerrainUnion = Union[Grass, Road, Swamp, PotholeRoad, Water]

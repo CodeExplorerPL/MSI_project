d37cdf1c7a134324505c18b374d79acebcd93cb0
BlahajEnjoyer <thewhiteshark@onet.pl>
2026-01-12 15:14:26 +0100

    api import fix

--- 02_FRAKCJA_SILNIKA/controller/api.py
+++ 02_FRAKCJA_SILNIKA/controller/api.py
@@ -1,13 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional, Dict, Literal, Any, Union
 from dataclasses import dataclass
-import sys
-import os
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-if parent_dir not in sys.path:
-    sys.path.append(parent_dir)
 
 # Importy z backendu
 from backend.structures.position import Position
